fe724be8318a1c60b3174b4901e4d5af48ca8e92
Tiago Fernandes Viana <tiagofviana.dev@gmail.com>
2026-07-14 15:04:04 -0300

    Changing field erros to accept a list of errors

---
+++ src/front-end/spa/components/form/FieldErrors.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul ref="list">
+        <li
+            v-for="(item, index) in messages"
+            :key="index"
+            class="table pl-1 text-sm text-red-600 italic"
+        >
+            {{ item }}
+        </li>
+    </ul>
+</template>
+
+<script setup lang="ts">
+import { useTemplateRef, onMounted } from 'vue'
+import gsap from 'gsap'
+
+defineProps<{ messages: Array<string> }>()
+
+const list = useTemplateRef<HTMLElement>('list')
+
+onMounted(() => {
+    gsap.fromTo(
+        list.value,
+        {
+            opacity: 0,
+        },
+        {
+            opacity: 1,
+            duration: 0.5,
+        },
+    )
+})
+</script>
