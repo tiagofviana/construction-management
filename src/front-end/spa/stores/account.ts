9967c4ebc34b71d2174389e09166763e438326f8
Tiago Fernandes Viana <tiagofviana.dev@gmail.com>
2026-07-14 14:56:03 -0300

    Removing auth store and updating account store

--- src/front-end/spa/stores/account.ts
+++ src/front-end/spa/stores/account.ts
@@ -6,7 +6,6 @@ export interface AccountInfo {
     lastName: string
     email: string
     isStaff: boolean
-    isEmailVerified: boolean
     lastLogin: Date
 }
 
@@ -14,42 +13,33 @@ export const accountStore = defineStore('account', () => {
     let info: AccountInfo | null = null
 
     async function getInfo(): Promise<AccountInfo | null> {
-        if (info === null) {
-            await axios.get('/api/account/info/get').then((response) => {
-                const lastLogin = new Date(response.data.lastLogin).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric',
-                    hour: '2-digit',
-                    minute: '2-digit',
-                })
+        if (info !== null) return info
 
-                info = {
-                    ...response.data,
-                    lastLogin: lastLogin,
-                }
-            })
+        const response = await axios.get('/api/account/info/get')
+
+        if (response.status === 204) {
+            return null
         }
+
+        const lastLogin = new Date(response.data.lastLogin).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+        })
+
+        info = {
+            ...response.data,
+            lastLogin: lastLogin,
+        }
+
         return info
-    }
-
-    async function isAccountVerified() {
-        const info = await getInfo()
-
-        if (info === null) {
-            return false
-        }
-
-        if (info.isEmailVerified === false) {
-            return false
-        }
-
-        return true
     }
 
     function reset() {
         info = null
     }
 
-    return { getInfo, isAccountVerified, reset }
+    return { getInfo, reset }
 })
