9266f65a170fdb94e711cc740b1ca001d9d96135
Tiago Fernandes Viana <tiagofviana.dev@gmail.com>
2026-07-17 22:29:27 -0300

    Creating simple loader component

---
+++ src/front-end/spa/components/loading/SimpleLoader.vue
@@ -0,0 +1,11 @@
+<template>
+    <LoaderCircle
+        class="table animate-spin drop-shadow-sm drop-shadow-black/20"
+        :stroke-width="3"
+        :size="26"
+    />
+</template>
+
+<script lang="ts" setup>
+import { LoaderCircle } from '@lucide/vue'
+</script>
