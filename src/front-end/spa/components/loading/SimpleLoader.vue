6edb3952729f471b299b5f4d6111a5317740c073
Tiago Fernandes Viana <tiagofviana.dev@gmail.com>
2026-07-19 21:55:00 -0300

    Adding props

--- src/front-end/spa/components/loading/SimpleLoader.vue
+++ src/front-end/spa/components/loading/SimpleLoader.vue
@@ -1,11 +1,24 @@
 <template>
     <LoaderCircle
         class="table animate-spin drop-shadow-sm drop-shadow-black/20"
-        :stroke-width="3"
-        :size="26"
+        :stroke-width="props.strokeWidth"
+        :size="props.size"
     />
 </template>
 
 <script lang="ts" setup>
 import { LoaderCircle } from '@lucide/vue'
+
+const props = defineProps({
+    size: {
+        type: Number,
+        required: false,
+        default: 26,
+    },
+    strokeWidth: {
+        type: Number,
+        required: false,
+        default: 3,
+    },
+})
 </script>
